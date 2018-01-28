1ed243877cca2ec21605c19af3f3138d9e0e93e8
KampusHub <verbelesteban2@gmail.com>
2018-01-28 14:16:23 -0500

    Added response DTO class

---
+++ SurveyServiceApp/DataObjects/SurveyResponse.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Mobile.Server;
+
+namespace SurveyServiceApp.DataObjects
+{
+    public class SurveyResponse : EntityData
+    {
+
+        public string QuestionId { get; set; }
+
+        public string Name { get; set; }
+
+        public int AnswerIndex { get; set; }
+        
+    }
+}
