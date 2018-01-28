fc2e0ce2f369ba30a06a0971d24396eef5c81088
KampusHub <verbelesteban2@gmail.com>
2018-01-28 14:39:17 -0500

    Added attributes to dto to map class with db and modify properties names

--- SurveyServiceApp/DataObjects/SurveyResponse.cs
+++ SurveyServiceApp/DataObjects/SurveyResponse.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.Azure.Mobile.Server;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SurveyServiceApp.DataObjects
 {
+    [Table("responses")] // this maps the class to the responses table on db
     public class SurveyResponse : EntityData
     {
-
+        [JsonProperty("quastionid")]
         public string QuestionId { get; set; }
 
         public string Name { get; set; }
 
+        [JsonProperty("answer")]
         public int AnswerIndex { get; set; }
         
     }
