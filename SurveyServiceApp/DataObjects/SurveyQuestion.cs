2ea5039548feb52bc83102ddc508e40f9fa6c0fa
KampusHub <verbelesteban2@gmail.com>
2018-01-28 15:19:59 -0500

    Added SurveyQuestion DTO class

---
+++ SurveyServiceApp/DataObjects/SurveyQuestion.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SurveyServiceApp.DataObjects
+{
+    [Table("questions")]
+    public class SurveyQuestion : EntityData
+    {
+
+        public string Text { get; set; }
+
+        public string Answers { get; set; }
+
+    }
+}
